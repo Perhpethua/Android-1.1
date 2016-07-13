94fb0233efbfb3b632cfed97911638226695e21d
Perhpethua <nikolina.alajbeg@gmail.com>
2016-07-14 00:01:23 +0200

    sqlite

--- platforms/android/assets/www/js/basesql.js
+++ platforms/android/assets/www/js/basesql.js
@@ -1,112 +1,128 @@
-document.addEventListener("deviceready", OnDeviceReady, false);
-document.getElementById ("submitbtn").addEventListener ("click", getplayers, false);
 
-var db, tx;
-var pname1, 
-    pname2, 
-    pname3, 
-    pname4, 
-    pname5, 
-    pname6, 
-    pname7, 
-    pname8;
+document.addEventListener("deviceready", OnDeviceReady, false);
+window.onload=function(){
+    document.getElementById("submitbtn").addEventListener ("click", getplayers, false);
+    document.getElementById("subb").addEventListener("click", getRecords, false);
+}
+var db, clickable = true;
+var imena = [null, null, null, null, null, null, null, null];
+
 function OnDeviceReady() {
     alert("device is ready!");
-//    window.sqlitePlugin.echoTest(successCallback, errorCallback);
-//    window.sqlitePlugin.selfTest(successCallback, errorCallback);
-    makeDB();
-}
-function getplayers(){
-    pname1 = document.getElementById("playersname1").value;
-    console.log(pname1);
-    pname2 = document.getElementById("playersname2").value;
-    console.log(pname2);
-    pname3 = document.getElementById("playersname3").value;
-    console.log(pname3);
-    pname4 = document.getElementById("playersname4").value;
-    console.log(pname4);
-    pname5 = document.getElementById("playersname5").value;
-    console.log(pname5);
-    pname6 = document.getElementById("playersname6").value;
-    console.log(pname6);
-    pname7 = document.getElementById("playersname7").value;
-    console.log(pname7);
-    pname8 = document.getElementById("playersname8").value; 
-    console.log(pname8);
 
-    //alert(pname1);
-    if(pname1=="" || pname2==""){
+    makeDB();
+    clickable = true;
+}
+function openDB()
+{
+    return window.openDatabase('playerDB', "1.0", "Default", 200000);
+}
+function makeDB(){ // otvaranje baze
+//({imebaze, lokacija, za koristenje built-in Andorid db klasa, workaround that simply closes and reopens the database file at the end of every transaction that is committed})
+    db = openDB();
+    db.transaction(fillDB, errorcb, successcb);
+
+    alert("User: makeDB");
+
+}
+function fillDB(tx){
+    tx.executeSql('CREATE TABLE IF NOT EXISTS player(id INTEGER PRIMARY KEY AUTOINCREMENT, name text)'); 
+}
+function successcb(){
+    alert("DB created Successfully!");    
+    //fillDB();
+}
+function errorcb(){
+    alert("Can't add player into DataBase!");
+    db.close(); // ako je doslo do greske zatvori bazu
+}
+function unesi(tx)
+{ // funkcija upisuje podatke u tablicu
+
+    db = openDB();
+    
+    alert("instert into table p1 p2");
+
+
+    var queri = 'INSERT INTO player (name) VALUES ("'+imena[0]+'")';
+    db.transaction(function (tx){
+        tx.executeSql(queri, [], defaultsuccess, defaultfault);
+    }, transactionfault, transactionsucces);
+
+    var queri1 = 'INSERT INTO player (name) VALUES ("'+imena[1]+'")';
+    db.transaction(function (tx){
+        tx.executeSql(queri1, [], defaultsuccess, defaultfault);
+    }, transactionfault, transactionsucces);
+}
+
+    //otvoristr();
+
+function defaultsuccess(){
+    alert("default success");
+}
+function defaultfault(error){
+    alert("default pogreska: " + error);
+}
+function transactionsucces(){
+    alert("Transaction success");
+}
+function transactionfault(){
+    alert("Transaction fault");
+}
+function otvoristr(){ // funckija otvara wheel.html 
+    
+    window.location.assign("wheel.html"); //works on android
+}
+
+function getRecords(){
+    db = openDB();
+    alert("getRecords");
+    db.transaction(function (tx){
+        tx.executeSql("SELECT * FROM player", [], resultSuccess, resultError);
+    }, transactionfault, transactionsucces );
+}
+function getRecSuccess(){
+    alert("getRecSuccess");
+}
+
+function resultSuccess(tx, response){
+    alert("Response : " + response);
+    var div = document.getElementById("divResponse");
+    var temp = "<table><tr><th>name</th></tr></table>";
+
+    alert(response.rows.length); //4
+    for (var i = 0; i < response.rows.length; i++) {
+        //append all strings
+        temp += "<button>" + response.rows.item(i).name+"</button>";
+        alert(temp);
+    }
+    div.innerHTML = temp;
+}
+function resultError(error){
+    alert("greska e: " + error);
+}
+
+
+
+function getplayers(){
+    if(!clickable)return;
+    var names = ["playersname1",
+                "playersname2",
+                "playersname3",
+                "playersname4",
+                "playersname5",
+                "playersname6",
+                "playersname7",
+                "playersname8" ]
+    for(var i = 0; i < 8; i++){
+        imena[i] = document.getElementById(names[i]).value;
+    }
+    
+        //alert(pname1);
+    if(imena[0]=="" || imena[1]==""){
         //alert("prazna polja");
         navigator.notification.alert("2 players are minimum!", null, "Fill up to continue", "Ok");                
     }else{
         unesi();
     }
-}
-function makeDB(){ // otvaranje baze
-    //alert("makeDB");
-//({imebaze, lokacija, za koristenje built-in Andorid db klasa, workaround that simply closes and reopens the database file at the end of every transaction that is committed})
-    db = window.sqlitePlugin.openDatabase({name: 'playerDB.db', location: 'default', androidDatabaseImplementation: 2, androidLockWorkaround: 1}, successcb, errorcb);
-   /* provjera: check
-   if (db) {
-        alert("db done");
-    };
-    */
-}
-function successcb(){
-    navigator.notification.alert("DB created Successfully!", null, "Information", "Ok");
-//kreiranje tablice ako je baza stvorena
-    db.transaction(function(tx) {
-        //alert("creating table");
-        tx.executeSql('DROP TABLE IF EXISTS player'); // brise tablicu ako postoji
-        tx.executeSql("CREATE TABLE IF NOT EXISTS player(id INTEGER PRIMARY KEY AUTOINCREMENT, name text)");     
-  });
-}
-function errorcb(){
-    navigator.notification.alert(error, null, "Can't add player into DataBase!\nDatabase closed!", "Ok");
-    db.close(); // ako je doslo do greske zatvori bazu
-}
-function unesi(tx){ // funkcija upisuje podatke u tablicu
-    db = window.sqlitePlugin.openDatabase({name: 'playerDB.db', location: 'default', androidDatabaseImplementation: 2, androidLockWorkaround: 1});
-    alert("instert into table p1 p2");
-
-    var qry1 = "INSERT INTO player (id, name)VALUES( 1, "+pname1+")";
-    //alert(qry1); // prikazuje
-    tx.executeSql(qry1);
-
-   
-    var qry2 = "INSERT INTO player (id, name)VALUES( 2, "+pname2+")";
-    //alert(qry2); //ne prikazuje
-    tx.executeSql(qry2);
-    
-    if(!(pname3 == "")){
-        alert("insert pname3");
-        tx.executeSql('INSERT INTO player (id, name)VALUES( 3, '+pname3+')');
-    }
-    if(!(pname4 == "")){
-        alert("insert pname4");
-        tx.executeSql('INSERT INTO player (id, name)VALUES( 4, '+pname4+')');
-    }
-    if(!(pname5 == "")){
-        alert("inser pname5");
-        tx.executeSql('INSERT INTO player (id, name)VALUES( 5, '+pname5+')');
-    }
-    if(!(pname6 == "")){
-        alert("insert pname6");
-        tx.executeSql('INSERT INTO player (id, name)VALUES( 6, '+pname6+')');
-    }
-    if(!(pname7 == "")){
-        alert("inser pname7");
-        tx.executeSql('INSERT INTO player (id, name)VALUES( 7, '+pname7+')');
-    }
-    if(!(pname8 == "")){
-        alert("insert pname8");
-        tx.executeSql('INSERT INTO player (id, name)VALUES( 8, '+pname8+')');
-    }else{
-        alert("Full capacity of players is not recorded.");
-    }
-    otvoristr();
-}
-function otvoristr(){ // funckija provjerava jesu li zadovoljeni uvjeti od 2 igraca minimalno
-    
-    window.location.assign("wheel.html"); //works on android
 }
---
+++ www/js/garbage.js
@@ -0,0 +1,38 @@
+garbage :
+
+   var qry3 = "INSERT INTO player (id, name)VALUES( 3, "+pname3+")";
+    if(!(pname3 == "")){
+        db.transaction(function (tx){
+            tx.executeSql(qry3, [], defaultsuccess, defaultfault);  
+            });  
+    }
+    var qry4 = "INSERT INTO player (id, name)VALUES( 4, "+pname4+")";
+    if(!(pname4 == "")){
+        db.transaction(function (tx){
+            tx.executeSql(qry4, [], defaultsuccess, defaultfault);    
+        });
+    }
+    var qry5 = "INSERT INTO player (id, name)VALUES( 5, "+pname5+")";
+    if(!(pname5 == "")){
+        db.transaction(function (tx){
+            tx.executeSql(qry5, [], defaultsuccess, defaultfault);
+            });    
+    }
+    var qry6 = "INSERT INTO player (id, name)VALUES( 6, "+pname6+")";
+    if(!(pname6 == "")){
+        db.transaction(function (tx){
+            tx.executeSql(qry6, [], defaultsuccess, defaultfault);    
+        });
+    }
+    var qry7 = "INSERT INTO player (id, name)VALUES( 7, "+pname7+")";
+    if(!(pname7 == "")){
+        db.transaction(function (tx){
+            tx.executeSql(qry7, [], defaultsuccess, defaultfault);    
+        });
+    }
+    var qry8 = "INSERT INTO player (id, name)VALUES( 8, "+pname8+")";
+    if(!(pname8 == "")){
+        db.transaction(function (tx){
+            tx.executeSql(qry8, [], defaultsuccess, defaultfault);    
+        });
+    }
