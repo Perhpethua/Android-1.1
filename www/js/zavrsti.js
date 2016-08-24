5b1ed8509c2b93f9f0e8472d08ca700385a339fb
Perhpethua <nikolina.alajbeg@gmail.com>
2016-08-25 00:51:33 +0200

    završni detalji

--- www/js/zavrsti.js
+++ www/js/zavrsti.js
@@ -15,7 +15,7 @@ $(document).ready(function () {
 function ide(){
 	//console.log("usli");
 	document.getElementById("back").style.display = "none";
-	document.body.style.backgroundColor = "#484848";//changes color when spinning
+	//document.body.style.backgroundColor = "#484848";//changes color when spinning
 	elem1 = document.getElementById("zuta");//zuta
 	elem2 = document.getElementById("crvena");//crvena
 	elem3 = document.getElementById("plava");//plava
